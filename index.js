43ce30504aab2839b8dc0a8951a91c84e16c4e12
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-05-01 22:58:45 +0200

    Asks for API key, saves in config file

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 import fs from "fs/promises";
 import { config } from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
 
 config();
 
-async function generateComments(filePath) {
-  const apiKey = process.env.API_KEY;
-
+async function generateComments(filePath, apiKey) {
   const openai = new OpenAIApi(
     new Configuration({
       apiKey: apiKey,
@@ -29,12 +28,45 @@ async function generateComments(filePath) {
   await fs.writeFile(filePath, response, "utf-8");
 }
 
-if (process.argv.length < 3) {
-  console.error("Please provide a file path.");
-  process.exit(1);
+const configFilePath = "commentGPTConfig.json";
+
+fs.readFile(configFilePath, "utf-8")
+  .then((configString) => JSON.parse(configString))
+  .then((config) => {
+    if (config.apiKey) {
+      generateComments(process.argv[2], config.apiKey)
+        .then(() => console.log("Comments generated successfully."))
+        .catch((error) => console.error("Error generating comments:", error))
+        .finally(() => rl.close());
+    } else {
+      askForApiKey();
+    }
+  })
+  .catch((error) => {
+    if (error.code === "ENOENT") {
+      askForApiKey();
+    } else {
+      console.error("Error reading configuration file:", error);
+    }
+  });
+
+function askForApiKey() {
+  rl.question("Please enter your OpenAI API key: ", (apiKey) => {
+    const config = { apiKey: apiKey };
+
+    fs.writeFile(configFilePath, JSON.stringify(config))
+      .then(() => console.log("API key saved to configuration file."))
+      .catch((error) => console.error("Error saving API key:", error))
+      .finally(() => {
+        generateComments(process.argv[2], apiKey)
+          .then(() => console.log("Comments generated successfully."))
+          .catch((error) => console.error("Error generating comments:", error))
+          .finally(() => rl.close());
+      });
+  });
 }
 
-const filePath = process.argv[2];
-generateComments(filePath)
-  .then(() => console.log("Comments generated successfully."))
-  .catch((error) => console.error("Error generating comments:", error));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
